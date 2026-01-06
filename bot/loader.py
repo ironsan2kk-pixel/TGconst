ed9406c9f53f22d2b06c7177d18b597fe707e3ed
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-06 22:07:21 +0300

    Fix: совместимость с разными версиями Aiogram 3.x

--- bot/loader.py
+++ bot/loader.py
@@ -3,21 +3,27 @@
 """
 
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.config import config
 
-
-# Создаём бота
-bot = Bot(
-    token=config.BOT_TOKEN,
-    default=DefaultBotProperties(
-        parse_mode=ParseMode.HTML,
-        link_preview_is_disabled=True,
+# Совместимость с разными версиями Aiogram 3.x
+try:
+    from aiogram.client.default import DefaultBotProperties
+    bot = Bot(
+        token=config.BOT_TOKEN,
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML,
+            link_preview_is_disabled=True,
+        )
     )
-)
+except ImportError:
+    # Для старых версий Aiogram 3.x
+    bot = Bot(
+        token=config.BOT_TOKEN,
+        parse_mode=ParseMode.HTML,
+    )
 
 # Создаём диспетчер
 storage = MemoryStorage()
