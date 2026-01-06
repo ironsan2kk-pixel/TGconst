8d5f60ae538cefa1a96a251b6ed4930a87bad030
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-06 22:01:19 +0300

    Чат 2: Callbacks init

--- bot/callbacks/__init__.py
+++ bot/callbacks/__init__.py
@@ -1,1 +1,17 @@
-"""Package."""
+"""
+Callback-обработчики бота.
+"""
+
+from aiogram import Router
+
+from bot.callbacks import language, tariff
+
+
+def setup_callbacks() -> Router:
+    """Настроить и вернуть роутер с callback-обработчиками."""
+    router = Router()
+    
+    router.include_router(language.router)
+    router.include_router(tariff.router)
+    
+    return router
