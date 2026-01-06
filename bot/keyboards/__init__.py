0b7a5bf727047b88982b354487807329840b74b8
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-06 22:00:58 +0300

    Чат 2: Keyboards init

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,1 +1,20 @@
-"""Package."""
+"""
+Клавиатуры бота.
+"""
+
+from bot.keyboards.inline import (
+    language_keyboard,
+    main_menu_keyboard,
+    tariffs_keyboard,
+    tariff_detail_keyboard,
+    back_to_menu_keyboard,
+)
+
+
+__all__ = [
+    'language_keyboard',
+    'main_menu_keyboard',
+    'tariffs_keyboard',
+    'tariff_detail_keyboard',
+    'back_to_menu_keyboard',
+]
