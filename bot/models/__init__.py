3a5ba924da63a5dbfc1c9d643094720f477a3e73
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-06 21:36:45 +0300

    Add bot/models/__init__.py

---
+++ bot/models/__init__.py
@@ -0,0 +1,32 @@
+"""SQLAlchemy models."""
+
+from .base import Base, TimestampMixin
+from .settings import Settings
+from .channel import Channel
+from .tariff import Tariff, TariffChannel
+from .user import User
+from .subscription import Subscription
+from .payment import Payment
+from .promocode import Promocode, PromocodeUse
+from .broadcast import Broadcast
+from .menu_item import MenuItem
+from .faq_item import FAQItem
+from .admin_log import AdminLog
+
+__all__ = [
+    "Base",
+    "TimestampMixin",
+    "Settings",
+    "Channel",
+    "Tariff",
+    "TariffChannel",
+    "User",
+    "Subscription",
+    "Payment",
+    "Promocode",
+    "PromocodeUse",
+    "Broadcast",
+    "MenuItem",
+    "FAQItem",
+    "AdminLog",
+]
