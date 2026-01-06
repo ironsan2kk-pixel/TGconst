4ad6bff33b84eb77ef2645a3eb37b2d60bcf4c6a
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-06 20:08:34 +0300

    Disable auth - frontend/src/api/client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -7,25 +7,6 @@ const client = axios.create({
   },
 })
 
-// Request interceptor - добавляем токен
-client.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
-})
-
-// Response interceptor - обработка 401
-client.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token')
-      window.location.href = '/login'
-    }
-    return Promise.reject(error)
-  }
-)
+// Авторизация отключена - токен не требуется
 
 export default client
