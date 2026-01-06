494ac15f1a8cbd78f839d61f0ce69dc1b996feba
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-06 22:01:20 +0300

    Чат 2: Language callbacks

---
+++ bot/callbacks/language.py
@@ -0,0 +1,11 @@
+"""
+Callback-обработчики языка.
+Основная логика в handlers/language.py
+"""
+
+from aiogram import Router
+
+router = Router()
+
+# Callbacks для языка обрабатываются в handlers/language.py
+# Этот файл для дополнительных callbacks при необходимости
