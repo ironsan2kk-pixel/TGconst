4b96bbb91c831b9b6d8ea8169793bb8a7eff5faa
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-07 07:59:37 +0300

    Чат 1: Add userbot/__init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,18 +1,1 @@
-"""
-Pyrogram Userbot для управления каналами.
-
-Функционал:
-- Добавление пользователей в каналы
-- Удаление пользователей из каналов
-- Проверка участия в каналах
-"""
-
-from .config import userbot_config
-from .client import userbot_client, get_userbot, UserbotClient
-
-__all__ = [
-    'userbot_config',
-    'userbot_client',
-    'get_userbot',
-    'UserbotClient',
-]
+"""Package."""
