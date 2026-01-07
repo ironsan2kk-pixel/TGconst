5b16e9ffae538a5c9260db2effe9cfaf1a86c626
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-07 07:59:23 +0300

    Чат 1: Add bot/middlewares/__init__.py

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,18 +1,1 @@
-"""
-Middleware для бота.
-"""
-
-from bot.middlewares.database import DatabaseMiddleware
-from bot.middlewares.user import UserMiddleware
-from bot.middlewares.i18n import I18nMiddleware
-from bot.middlewares.ban import BanMiddleware
-from bot.middlewares.rate_limit import RateLimitMiddleware
-
-
-__all__ = [
-    'DatabaseMiddleware',
-    'UserMiddleware',
-    'I18nMiddleware',
-    'BanMiddleware',
-    'RateLimitMiddleware',
-]
+"""Package."""
