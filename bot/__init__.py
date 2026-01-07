c68edb67affe8900933b44561804f9da0090e343
ironsan2kk-pixel <ironsan2kk@gmail.com>
2026-01-07 07:59:19 +0300

    Чат 1: Add bot/__init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,9 +1,1 @@
-"""
-Telegram бот для продажи доступа к каналам.
-"""
-
-from bot.config import config
-from bot.loader import bot, dp
-
-
-__all__ = ['bot', 'dp', 'config']
+"""Telegram bot package."""
